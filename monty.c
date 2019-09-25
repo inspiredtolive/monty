431b044bce24778c2f988cee69e9cd22a5da07d5
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-09-25 23:09:05 +0000

    reads and execute monty scripts

---
+++ monty.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+allocated_t mem;
+
+/**
+ * main - opens a monty script file and executes it
+ * @argc: number of arguments, should be 2
+ * @argv: array of command line arguments, should contain path to monty script
+ * Return: Success(0), Error(EXIT_FAILURE)
+ */
+int main(int argc, char const *argv[])
+{
+	/* Checks for file arguments */
+	if (argc == 1)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
+
+	/* Attempts to open the file */
+	mem.pScript = fopen(argv[1], "r");
+	if (mem.pScript == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
+
+	/* Reads each line of the file */
+	execute_script();
+	return (0);
+}
---
+++ monty.h
@@ -0,0 +1,70 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct allocated_s - global struct to contain memory to free and push value
+ * @n: integer value for the push opcode
+ * @token: tokenized opcode to free
+ * @pScript: file pointer to Monty bytecode file
+ */
+typedef struct allocated_s
+{
+	char *n;
+	char *token;
+	FILE *pScript;
+} allocated_t;
+
+extern allocated_t mem;
+void execute_script(void);
+void stack_push(stack_t **stack, unsigned int line_number);
+void stack_pall(stack_t **stack, unsigned int line_number);
+void stack_pint(stack_t **stack, unsigned int line_number);
+void stack_pop(stack_t **stack, unsigned int line_number);
+void stack_swap(stack_t **stack, unsigned int line_number);
+void stack_add(stack_t **stack, unsigned int line_number);
+void stack_nop(stack_t **stack, unsigned int line_number);
+void stack_sub(stack_t **stack, unsigned int line_number);
+void stack_div(stack_t **stack, unsigned int line_number);
+void stack_mul(stack_t **stack, unsigned int line_number);
+void stack_mod(stack_t **stack, unsigned int line_number);
+void stack_pchar(stack_t **stack, unsigned int line_number);
+void stack_pstr(stack_t **stack, unsigned int line_number);
+void stack_rotl(stack_t **stack, unsigned int line_number);
+void stack_rotr(stack_t **stack, unsigned int line_number);
+void free_all(stack_t *stack);
+
+#endif
