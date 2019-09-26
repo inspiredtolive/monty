84d7813120f964fc6191cde67be11422eb7dc634
Marco Chan <inspiredtolive0@gmail.com>
2019-09-26 00:48:46 +0000

    Prints correct error message

--- stack_div.c
+++ stack_div.c
@@ -11,7 +11,7 @@ void stack_div(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 		free_all(*stack);
 		exit(EXIT_FAILURE);
 	}
